47b5b21b8e469470092cc4880dfc6f0f5836e5c8
AndreaMeroi <andreameroidublin@gmail.com>
2025-11-13 10:33:28 +0100

    Add exercise

---
+++ routes/posts.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+const posts = [
+    {
+        ID: 1,
+        titolo: 'ciambellone',
+        contenuto: 'la ricetta del ciambellone',
+        img: 'imgs/ciambellone.jpeg',
+        tags: ['ciambellone', "torta", "dolce", "zucchero"]
+
+    }, {
+        ID: 2,
+        titolo: 'cracker_barbabietola',
+        contenuto: 'la ricetta del cracker_barbabietola',
+        img: 'imgs/cracker_barbabietola.jpeg',
+        tags: ['cracker_barbabietola', "torta", "dolce", "zucchero"]
+    }, {
+        ID: 3,
+        titolo: 'pane_fritto_dolce',
+        contenuto: 'la ricetta del pane_fritto_dolce',
+        img: 'imgs/pane_fritto_dolce.jpeg',
+        tags: ['pane_fritto_dolce', "torta", "dolce", "zucchero"]
+    }, {
+        ID: 4,
+        titolo: 'pasta_barbabietola',
+        contenuto: 'la ricetta del pasta_barbabietola',
+        img: 'imgs/pasta_barbabietola.jpeg',
+        tags: ['pasta_barbabietola', "torta", "dolce", "zucchero"]
+    }, {
+        ID: 5,
+        titolo: 'torta_paesana',
+        contenuto: 'la ricetta del torta_paesana',
+        img: 'imgs/torta_paesana.jpeg',
+        tags: ['torta_paesana', "torta", "dolce", "zucchero"]
+    }
+
+]
+
+
+
+//routes 
+
+app.get('/', (req, res) => {
+    res.send('<h1>Ciambellone</h1><img src="http://localhost:3002/imgs/ciambellone.jpeg" alt= "welcome to the panetteria">')
+})
+
+// Routes Group 
+
+//INDEX
+
+app.get('/', (req, res) => {
+
+    res.json('Lista dei posts')
+})
+
+//SHOW
+
+app.get('/:id', (req, res) => {
+    res.send('Visulaizza il post con ID:' + req.params.id)
+})
+
+//STORE
+
+app.post('/', (req, res) => {
+    res.send('Aggiungi nuovo post')
+})
+//UPDATE
+
+app.put('/:id', (req, res) => {
+    res.send('modifica completamente il post con ID:' + req.params.id)
+})
+
+//MODIFY
+
+app.patch('/:id', (req, res) => {
+    res.send('modifica parzialmente il post con ID:' + req.params.id)
+})
+
+//DESTROY
+
+app.delete('/:id', (req, res) => {
+    res.send('cancella il post con ID:' + req.params.id)
+
+})
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3002
+const postsRouter = require('./routes/posts')
 
 //set static folder
 app.use(express.static('public'))
@@ -12,85 +13,7 @@ app.listen(port, () => {
 
 })
 
+// Routes Group 
 
+app.use('./routes/posts.js', postsRouter)
 
-const posts = [
-    {
-        ID: 1,
-        titolo: 'ciambellone',
-        contenuto: 'la ricetta del ciambellone',
-        img: 'imgs/ciambellone.jpeg',
-        tags: ['ciambellone', "torta", "dolce", "zucchero"]
-
-    }, {
-        ID: 2,
-        titolo: 'cracker_barbabietola',
-        contenuto: 'la ricetta del cracker_barbabietola',
-        img: 'imgs/cracker_barbabietola.jpeg',
-        tags: ['cracker_barbabietola', "torta", "dolce", "zucchero"]
-    }, {
-        ID: 3,
-        titolo: 'pane_fritto_dolce',
-        contenuto: 'la ricetta del pane_fritto_dolce',
-        img: 'imgs/pane_fritto_dolce.jpeg',
-        tags: ['pane_fritto_dolce', "torta", "dolce", "zucchero"]
-    }, {
-        ID: 4,
-        titolo: 'pasta_barbabietola',
-        contenuto: 'la ricetta del pasta_barbabietola',
-        img: 'imgs/pasta_barbabietola.jpeg',
-        tags: ['pasta_barbabietola', "torta", "dolce", "zucchero"]
-    }, {
-        ID: 5,
-        titolo: 'torta_paesana',
-        contenuto: 'la ricetta del torta_paesana',
-        img: 'imgs/torta_paesana.jpeg',
-        tags: ['torta_paesana', "torta", "dolce", "zucchero"]
-    }
-
-]
-
-
-//routes 
-
-app.get('/', (req, res) => {
-    res.send('<h1>Ciambellone</h1><img src="http://localhost:3002/imgs/ciambellone.jpeg" alt= "welcome to the panetteria">')
-})
-
-
-//INDEX
-
-app.get('/api/bacheca', (req, res) => {
-
-    res.json('Lista dei posts')
-})
-
-//SHOW
-
-app.get('/api/bacheca/:id', (req, res) => {
-    res.send('Visulaizza il post con ID:' + req.params.id)
-})
-
-//STORE
-
-app.post('/api/bacheca', (req, res) => {
-    res.send('Aggiungi nuovo post')
-})
-//UPDATE
-
-app.put('/api/bacheca/:id', (req, res) => {
-    res.send('modifica completamente il post con ID:' + req.params.id)
-})
-
-//MODIFY
-
-app.patch('/api/bacheca/:id', (req, res) => {
-    res.send('modifica parzialmente il post con ID:' + req.params.id)
-})
-
-//DESTROY
-
-app.delete('/api/bacheca/:id', (req, res) => {
-    res.send('cancella il post con ID:' + req.params.id)
-
-})
